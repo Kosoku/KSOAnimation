59f4f6ab165e679511083662c0dcc0ca60050885
William Towe <willbur1984@gmail.com>
2017-08-19 16:08:54 -0400

    Initial Commit

---
+++ KSOAnimation/KSOAnimation.h
@@ -0,0 +1,19 @@
+//
+//  KSOAnimation.h
+//  KSOAnimation
+//
+//  Created by William Towe on 8/19/17.
+//  Copyright © 2017 Kosoku Interactive, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KSOAnimation.
+FOUNDATION_EXPORT double KSOAnimationVersionNumber;
+
+//! Project version string for KSOAnimation.
+FOUNDATION_EXPORT const unsigned char KSOAnimationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KSOAnimation/PublicHeader.h>
+
+
